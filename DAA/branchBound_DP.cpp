db05e84f03103489d10eee06bb7d6921d0ed7eee
Durga Prasad <65699516+dpgaharwal@users.noreply.github.com>
2024-11-11 00:57:05 +0530

    Update branchBound_DP.cpp

--- DAA/branchBound_DP.cpp
+++ DAA/branchBound_DP.cpp
@@ -1,5 +1,4 @@
-/*
-TC:O(2^N) SC: O(N)
+/*TC:O(2^N) SC: O(N)
 
     W:12
     P  30 28 20 24
@@ -130,67 +129,197 @@ int main()
     return 0;
 }
 */
-
 /* TC: O(N*W) SC: O(W)
 int profit[] = { 60, 100, 120 };
     int weight[] = { 10, 20, 30 };
     int W = 50;
 */
+/*
+****************LB*************************
 #include <bits/stdc++.h>
 using namespace std;
 
-// Function to find the maximum profit
-int knapSack(int W, int wt[], int val[], int n)
+int solve(vector<int> &weight, vector<int> &value, int index, int capacity)
 {
-    // Making and initializing dp array
-    int dp[W + 1];
-    memset(dp, 0, sizeof(dp));
-
-    for (int i = 1; i < n + 1; i++)
+    // base case
+    if (index == 0)
     {
-        for (int w = W; w >= 0; w--)
-        {
-            if (wt[i - 1] <= w)
-                // Finding the maximum value
-                dp[w] = max(dp[w], dp[w - wt[i - 1]] + val[i - 1]);
-        }
+        if (weight[0] <= capacity)
+            return value[0];
+        else
+            return 0;
     }
-    // Returning the maximum value of knapsack
-    return dp[W];
+
+    int include = 0;
+    if (weight[index] <= capacity)
+    {
+        include = value[index] + solve(weight, value, index - 1, capacity - weight[index]);
+    }
+
+    int exclude = solve(weight, value, index - 1, capacity);
+
+    int ans = max(exclude, include);
+
+    return ans;
 }
 
-// Driver code
+int solveMem(vector<int> &weight, vector<int> &value, int index, int capacity, vector<vector<int>> &dp)
+{
+    // base case
+    if (index == 0)
+    {
+        if (weight[0] <= capacity)
+            return value[0];
+        else
+            return 0;
+    }
+
+    if (dp[index][capacity] != -1)
+        return dp[index][capacity];
+    int include = 0;
+    if (weight[index] <= capacity)
+    {
+        include = value[index] + solveMem(weight, value, index - 1, capacity - weight[index], dp);
+    }
+
+    int exclude = solveMem(weight, value, index - 1, capacity, dp);
+
+    dp[index][capacity] = max(exclude, include);
+
+    return dp[index][capacity];
+}
+
+int solveTab(vector<int> &weight, vector<int> &value, int n, int capacity)
+{
+    // step 1:
+    vector<vector<int>> dp(n, vector<int>(capacity + 1, 0));
+
+    // step 2
+    for (int w = weight[0]; w <= capacity; w++)
+    {
+        if (weight[0] <= capacity)
+            dp[0][w] = value[0];
+        else
+            dp[0][w] = 0;
+    }
+
+    for (int index = 1; index < n; index++)
+    {
+        for (int w = 0; w <= capacity; w++)
+        {
+            int include = 0;
+            if (weight[index] <= w)
+            {
+                include = value[index] + dp[index - 1][w - weight[index]];
+            }
+
+            int exclude = 0 + dp[index - 1][w];
+
+            dp[index][w] = max(exclude, include);
+        }
+    }
+    return dp[n - 1][capacity];
+}
+
+
+int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight)
+{
+    // return solve(weight, value, n - 1, maxWeight);
+    // vector<vector<int>> dp(n, vector<int>(maxWeight + 1, -1));
+    // return solveMem(weight, value, n - 1, maxWeight, dp);
+    return solveTab(weight, value, n, maxWeight);
+}
+
 int main()
 {
-    int n, W;
-
+    int n;
     cout << "Enter the number of items: ";
     cin >> n;
 
-    int *profit = new int[n];
-    int *weight = new int[n];
+    vector<int> weight(n), value(n);
 
-    cout << "Enter the profit of each item:\n";
+    cout << "Enter the weights of the items: ";
     for (int i = 0; i < n; i++)
     {
-        cout << "Profit of item " << i + 1 << ": ";
-        cin >> profit[i];
-    }
-
-    cout << "Enter the weight of each item:\n";
-    for (int i = 0; i < n; i++)
-    {
-        cout << "Weight of item " << i + 1 << ": ";
         cin >> weight[i];
     }
 
-    cout << "Enter the maximum capacity of the knapsack: ";
-    cin >> W;
+    cout << "Enter the values of the items: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> value[i];
+    }
 
-    cout << "Maximum possible profit = " << knapSack(W, weight, profit, n);
+    int maxWeight;
+    cout << "Enter the maximum weight capacity of the knapsack: ";
+    cin >> maxWeight;
 
-    delete[] profit; // Free dynamically allocated memory
-    delete[] weight;
+    int maxProfit = knapsack(weight, value, n, maxWeight);
+    cout << "The maximum profit is: " << maxProfit << endl;
+
+    return 0;
+}
+*/
+
+/*Classic DP*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int knapsack(const vector<int> &weights, const vector<int> &values, int capacity)
+{
+    int n = weights.size();
+    vector<vector<int>> dp(n + 1, vector<int>(capacity + 1, 0));
+
+    // Filling the DP table
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int w = 1; w <= capacity; ++w)
+        {
+            if (weights[i - 1] <= w)
+            {
+                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1]);
+            }
+            else
+            {
+                dp[i][w] = dp[i - 1][w];
+            }
+        }
+    }
+
+    return dp[n][capacity];
+}
+
+int main()
+{
+    int n, capacity;
+
+    // Taking input from the user
+    cout << "Enter the number of items: ";
+    cin >> n;
+
+    vector<int> weights(n);
+    vector<int> values(n);
+
+    cout << "Enter the weights of the items: ";
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> weights[i];
+    }
+
+    cout << "Enter the values of the items: ";
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> values[i];
+    }
+
+    cout << "Enter the capacity of the knapsack: ";
+    cin >> capacity;
+
+    // Finding the maximum value in the knapsack
+    int result = knapsack(weights, values, capacity);
+
+    cout << "Maximum value in Knapsack = " << result << endl;
 
     return 0;
 }
